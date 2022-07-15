6662ea4dd08441a748f7cc1b64b8de9f8ec341b2
rianjd <rianjunckes@gmail.com>
2022-07-15 13:23:55 -0300

    alteração-mail

--- resources/views/sender/mail.blade.php
+++ resources/views/sender/mail.blade.php
@@ -1,5 +1,5 @@
-@if ($data['tipo'] == 'chatForm')<h2> Novo Chat:</h2>@endif
-@if ($data['tipo'] == 'coreForm')<h2> Novo usuario Core:</h2>@endif
+@if ($data['tipo'] == 'chatForm')<a class="navbar-brand" href="#"><img id="corebusiness_1200x300" src={{"https://github.com/rianjd/forms-cda/blob/master/public/images/logo-mattermost.png?raw=true"}} width="50" height="50" style="display:block;" alt=""><h2> Solicitação de Chat:</h2></a>@endif
+@if ($data['tipo'] == 'coreForm')<a class="navbar-brand" href="#"><img id="corebusiness_1200x300" src={{"https://github.com/rianjd/forms-cda/blob/master/public/images/corebusiness_1200x300.png?raw=true"}} width="200" height="50" style="display:block;" alt=""></a><h2> Novo usuario Core:</h2>@endif
 @if ($data['tipo'] == 'djForm')<h2> Novo usuario DJ:</h2>@endif
 @if ($data['tipo'] == 'emailForm')<h2> Criar novo email:</h2>@endif
 
@@ -12,7 +12,6 @@
 @if (isset($data['msg']))<p><strong>Observações: </strong>{{$data['msg'] }}</p>@endif
 
 
-
-@if ($data['tipo'] == 'chatForm')<p>Clique<a href="https://messenger.casasdaagua.com.br/signup_email?id=hrz53gtnrpyo3gux6pf1e9ierr"> AQUI </a> para criar um novo usuario. </p>@endif
+@if ($data['tipo'] == 'chatForm')<p><br>Clique<a href="https://messenger.casasdaagua.com.br/signup_email?id=hrz53gtnrpyo3gux6pf1e9ierr"> AQUI </a> para criar um novo usuario. </p>@endif
 @if ($data['tipo'] == 'coreForm')<p>Clique<a href="https://core.casasdaagua.com.br"> AQUI </a> para criar um novo usuario. </p>@endif
 
